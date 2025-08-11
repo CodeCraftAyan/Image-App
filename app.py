e335ce585fe91e36e8e1e397c7eff6289c9b192c
Ayan <ayan.mandal.work@gmail.com>
2025-08-11 23:39:34 +0530

    update code

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
+
 # Model
 class Photo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
